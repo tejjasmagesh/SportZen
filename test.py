6eaa7b57c66812ac5aaf1b1800491897d4dce6bc
tejjasmagesh <84600136+tejjasmagesh@users.noreply.github.com>
2022-11-01 19:46:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from flask import Flask,redirect,url_for,render_template,request,session,flash
-import nltk
-from nltk.chat.util import Chat, reflections
+
 import mysql.connector as m
 con=m.connect(host='localhost',user='root',passwd='mag26751!',database='hello')
 
@@ -17,66 +16,6 @@ app.permanent_session_lifetime = timedelta(minutes=10)
 
 
 
-reflections = {
-  "i am"       : "you are",
-  "i was"      : "you were",
-  "i"          : "you",
-  "i'm"        : "you are",
-  "i'd"        : "you would",
-  "i've"       : "you have",
-  "i'll"       : "you will",
-  "my"         : "your",
-  "you are"    : "I am",
-  "you were"   : "I was",
-  "you've"     : "I have",
-  "you'll"     : "I will",
-  "your"       : "my",
-  "yours"      : "mine",
-  "you"        : "me",
-  "me"         : "you"
-}
-pairs = [
-    [
-        r"my name is (.*)",
-        ["Hello %1, How are you today ?",]
-    ],
-    [
-        r"hi|hey|hello",
-        ["Hello", "Hey there",]
-    ], 
-    [
-        r"what is your name ?",
-        ["I am a bot created by Sports-Zen. You can call me BEE!",]
-    ],
-    [
-        r"I would like to book an arena ?",
-        ["which arena would you like to book?",]
-    ],
-    [
-        r"i'm (.*) doing good",
-        ["Nice to hear that","Do you need anything else?:)",]
-    ],
-    [
-        r"what (.*) want ?",
-        ["Make me an offer I can't refuse",]
-    ],
-    [
-        r"(.*) created ?",
-        ["Someone created me using Python's NLTK library ","top secret ;)",]
-    ],
-    [
-        r"(.*) (location|city) ?",
-        ['Bangalore, Karnataka',]
-    ],
-    [
-        r"how is weather in (.*)?",
-        ["Weather in %1 is awesome like always","Too hot man here in %1","Too cold man here in %1","Never even heard about %1"]
-    ],
-    [
-        r"(.*)raining in (.*)",
-        ["No rain since last week here in %2"]
-    ],
-    ]
 
 #
 #
@@ -90,16 +29,8 @@ def home():
 
 	if request.method=='POST':
 
-	    print("Hi! I am a chatbot created by SportZen for your service")
-	    chat = Chat(pairs, reflections)
-	    while True:
-	        msg = request.form['msg']
-	        y=chat.respond(msg)
-	        return render_template("index.html",response=y)
-	        if msg in 'byeGoodbye':
-	            y=chat.respond(msg)
-	            return render_template("index.html",response=y)
-	            break
+	    print("Hi! ")
+
 	else:
 		return render_template("index.html" )
 
